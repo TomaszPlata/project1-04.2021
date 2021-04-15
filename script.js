a8155d3408a07bfdb2c0d402315e86d077f5873f
Tom Bombadil <bombadilo@com.uk>
2021-04-15 16:27:04 +0200

    deleted script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-alert('Click that');
-
-const user_input = true;
-
-if (user_input==true) {
-    document.getElementById('attention').innerHTML = "Everything's gonna be fine, Jesus is with you^^!";
-} else {
-    document.getElementById('attention').innerHTML = "You need to rewrite your code";
-}

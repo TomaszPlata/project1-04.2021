936339a378ee61bfe58386948313f2bf4bc299ea
Tom Bombadil <bombadilo@com.uk>
2021-04-15 16:25:19 +0200

    added new line of code

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 alert('Click that');
 
-document.getElementById('attention').innerHTML = "Everything's gonna be fine, Jesus is with you^^!";
+const user_input = true;
+
+if (user_input==true) {
+    document.getElementById('attention').innerHTML = "Everything's gonna be fine, Jesus is with you^^!";
+} else {
+    document.getElementById('attention').innerHTML = "You need to rewrite your code";
+}
